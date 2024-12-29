82cb11d2ccffdff215608ed700e522b03b4fdf9d
NAbinav <abinav0506@gmail.com>
2024-12-29 22:50:42 +0530

    removing unwanted

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 // import { store } from "./redux/store";
-import { Provider } from "react-redux";
 
 const App = ({ children }: { children: React.ReactNode }) => {
   return <></>;
